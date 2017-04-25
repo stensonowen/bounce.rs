ea336d8c01fac4718121a739ef7de171b1fd5b30
owen <owen@oms.sh>
2017-04-25 14:00:33 -0400

    pong works, cleaned up a bit

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,22 @@ extern crate tokio_io;
 extern crate bytes;
 
 use std::str;
-use std::io::{self, ErrorKind, Write};
+use std::io;
+//use std::io::{self, ErrorKind, Write};
 use std::net::ToSocketAddrs;
 
 use futures::{future, Future, BoxFuture};
 use futures::{Stream, Poll, Async};
 use futures::{Sink, AsyncSink, StartSend};
-//use tokio_proto::TcpServer;
 use tokio_proto::TcpClient;
-//use tokio_proto::pipeline::ServerProto;
 use tokio_proto::pipeline::ClientProto;
 use tokio_service::Service;
 use tokio_core::reactor::Core;
 use tokio_io::codec::{Encoder, Decoder};
 use tokio_io::codec::Framed;
 use tokio_io::{AsyncRead, AsyncWrite};
-use bytes::{BytesMut, BufMut};
+//use bytes::{BytesMut, BufMut};
+use bytes::BytesMut;
 
 
 #[derive(Default)]
@@ -81,18 +81,16 @@ impl<T> Stream for PingPong<T>
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Option<String>, io::Error> {
-        loop {
-            // Poll the upstream transport. `try_ready!` will bubble up errors
-            // and Async::NotReady.
-            match try_ready!(self.upstream.poll()) {
-                Some(ref msg) if msg.starts_with("PING ") => { 
-                    // Intercept [ping] messages
-                    self.server = Some(msg[4..].to_owned());
-                    // Try flushing the pong, only bubble up errors
-                    try!(self.poll_complete());
-                }
-                m => return Ok(Async::Ready(m)),
+        // Poll the upstream transport
+        match try_ready!(self.upstream.poll()) {
+            Some(ref msg) if msg.starts_with("PING ") => { 
+                // Intercept pings
+                self.server = Some(msg[5..].to_owned());
+                // what's this ↓ ?
+                // Try flushing the pong, only bubble up errors
+                Ok(Async::Ready(try_ready!(self.upstream.poll())))
             }
+            m => Ok(Async::Ready(m))
         }
     }
 }
@@ -105,39 +103,17 @@ impl<T> Sink for PingPong<T>
 
     fn start_send(&mut self, item: String) -> StartSend<String, io::Error> {
         // Only accept the write if there are no pending pongs
-        //if self.pongs_remaining > 0 {
         if self.server.is_some() {
             return Ok(AsyncSink::NotReady(item));
         }
-
-        // If there are no pending pongs, then send the item upstream
         self.upstream.start_send(item)
     }
 
     fn poll_complete(&mut self) -> Poll<(), io::Error> {
-        //while self.pongs_remaining > 0 {
-		//while let Some(s) = self.server {
-		while self.server.is_some() {
-			let s = self.server.take();
-            // Try to send the pong upstream
-            let res = try!(self.upstream.start_send("[pong]".to_string()));
-
-            if !res.is_ready() {
-                // The upstream is not ready to accept new items
-                break;
-            }
+        if let Some(s) = self.server.take() {
+            let pong = format!("PONG {}", s);
+            self.upstream.start_send(pong)?;
         }
-
-        // Call poll_complete on the upstream
-        //
-        // If there are remaining pongs to send, this call may create additional
-        // capacity. One option could be to attempt to send the pongs again.
-        // However, if a `start_send` returned NotReady, and this poll_complete
-        // *did* create additional capacity in the upstream, then *our*
-        // `poll_complete` will get called again shortly.
-        //
-        // Hopefully this makes sense... it probably doesn't, so please ask
-        // questions in the Gitter channel and help me explain this better :)
         self.upstream.poll_complete()
     }
 }
@@ -147,13 +123,8 @@ impl<T> Sink for PingPong<T>
 pub struct LineProto;
 
 impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for LineProto {
-    /// For this protocol style, `Request` matches the `Item` type of the codec's `Encoder`
     type Request = String;
-
-    /// For this protocol style, `Response` matches the `Item` type of the codec's `Decoder`
     type Response = String;
-
-    /// A bit of boilerplate to hook in the codec:
     type Transport = PingPong<Framed<T, LineCodec>>;
     type BindTransport = Result<Self::Transport, io::Error>;
     fn bind_transport(&self, io: T) -> Self::BindTransport {
@@ -165,19 +136,12 @@ impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for LineProto {
 pub struct Echo;
 
 impl Service for Echo {
-    // These types must match the corresponding protocol types:
     type Request = String;
     type Response = String;
-
-    // For non-streaming protocols, service errors are always io::Error
     type Error = io::Error;
-
-    // The future for computing the response; box it for simplicity.
     type Future = BoxFuture<Self::Response, Self::Error>;
 
-    // Produce a future for computing a response from a request.
     fn call(&self, req: Self::Request) -> Self::Future {
-        // In this case, the response is immediate.
         future::ok(req).boxed()
     }
 }
@@ -209,164 +173,3 @@ fn main() {
     //println!("{}", String::from_utf8_lossy(&data));
 }
 
-/*
-extern crate futures;
-extern crate native_tls;
-extern crate tokio_core;
-extern crate tokio_io;
-extern crate tokio_tls;
-
-use std::io;
-use std::net::ToSocketAddrs;
-
-use futures::Future;
-use native_tls::TlsConnector;
-use tokio_core::net::TcpStream;
-use tokio_core::reactor::Core;
-use tokio_tls::TlsConnectorExt;
-
-
-fn main() {
-    let cm = "USER qj 0 * qjkx\r\nNICK qjk\r\nJOIN #test\r\n".as_bytes();
-
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let addr = "irc.freenode.org:6697".to_socket_addrs().unwrap().next().unwrap();
-
-    let cx = TlsConnector::builder().unwrap().build().unwrap();
-    let socket = TcpStream::connect(&addr, &handle);
-
-    let tls_handshake = socket.and_then(|socket| {
-        println!("Shaking");
-        let tls = cx.connect_async("irc.freenode.org", socket);
-        tls.map_err(|e| {
-            io::Error::new(io::ErrorKind::Other, e)
-        })
-    });
-    let request = tls_handshake.and_then(|socket| {
-        println!("Writing connect message");
-        tokio_io::io::write_all(socket, cm)
-    });
-    let response = request.and_then(|(socket, _request)| {
-        tokio_io::io::read_to_end(socket, vec![])
-    });
-
-    let (_socket, data) = core.run(response).unwrap();
-    println!("{}", String::from_utf8_lossy(&data));
-}
-
-*/
-
-
-/*
-
-extern crate futures;
-extern crate tokio_proto;
-extern crate tokio_service;
-extern crate tokio_io;
-extern crate bytes;
-
-use std::str;
-use std::io::{self, ErrorKind, Write};
-
-use futures::{future, Future, BoxFuture};
-use tokio_proto::TcpServer;
-use tokio_proto::pipeline::ServerProto;
-use tokio_service::Service;
-use tokio_io::codec::{Encoder, Decoder};
-use tokio_io::codec::Framed;
-use tokio_io::{AsyncRead, AsyncWrite};
-use bytes::{BytesMut, BufMut};
-
-// First, we implement a *codec*, which provides a way of encoding and
-// decoding messages for the protocol. See the documentation for `Codec` in
-// `tokio-core` for more details on how that works.
-
-#[derive(Default)]
-pub struct IntCodec;
-
-fn parse_u64(from: &[u8]) -> Result<u64, io::Error> {
-    Ok(str::from_utf8(from)
-       .map_err(|e| io::Error::new(ErrorKind::InvalidData, e))?
-       .parse()
-       .map_err(|e| io::Error::new(ErrorKind::InvalidData, e))?)
-}
-
-impl Decoder for IntCodec {
-    type Item = u64;
-    type Error = io::Error;
-
-    // Attempt to decode a message from the given buffer if a complete
-    // message is available; returns `Ok(None)` if the buffer does not yet
-    // hold a complete message.
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<u64>, io::Error> {
-        if let Some(mut i) = buf.iter().position(|&b| b == b'\n') {
-            // remove the line, including the '\n', from the buffer
-            let full_line = buf.split_to(i + 1);
-            // strip the `\n' (and `\r' if present)
-            if full_line.ends_with(&[b'\r', b'\n']) {
-                i -= 1;
-            }
-            let slice = &full_line[..i];
-            Ok(Some(parse_u64(slice)?))
-        } else {
-            Ok(None)
-        }
-    }
-
-    // Attempt to decode a message assuming that the given buffer contains
-    // *all* remaining input data.
-    fn decode_eof(&mut self, buf: &mut BytesMut) -> Result<Option<u64>, io::Error> {
-        let amt = buf.len();
-        Ok(Some(parse_u64(&buf.split_to(amt)[..])?))
-    }
-}
-
-impl Encoder for IntCodec {
-    type Item = u64;
-    type Error = io::Error;
-
-    fn encode(&mut self, item: u64, into: &mut BytesMut) -> io::Result<()> {
-        writeln!(into.writer(), "{}", item)?;
-        Ok(())
-    }
-}
-
-// Next, we implement the server protocol, which just hooks up the codec above.
-
-pub struct IntProto;
-
-impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for IntProto {
-    type Request = u64;
-    type Response = u64;
-    type Transport = Framed<T, IntCodec>;
-    type BindTransport = Result<Self::Transport, io::Error>;
-
-    fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(IntCodec))
-    }
-}
-
-// Now we implement a service we'd like to run on top of this protocol
-
-pub struct Doubler;
-
-impl Service for Doubler {
-    type Request = u64;
-    type Response = u64;
-    type Error = io::Error;
-    type Future = BoxFuture<u64, io::Error>;
-
-    fn call(&self, req: u64) -> Self::Future {
-        // Just return the request, doubled
-        future::finished(req * 2).boxed()
-    }
-}
-
-// Finally, we can actually host this service locally!
-fn main() {
-    let addr = "0.0.0.0:12345".parse().unwrap();
-    TcpServer::new(IntProto, addr)
-        .serve(|| Ok(Doubler));
-}
-*/
