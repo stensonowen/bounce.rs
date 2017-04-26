0fe211ebd62b5d7276c6c7eb9097d575178d53a3
owen <owen@oms.sh>
2017-04-25 23:03:35 -0400

    Code cleaned up, client shouldn't die
    Still have to stop sending ACKs and retry on connections failure
    I'm honestly surprised I managed to keep all lines < 80 chars in length

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ extern crate bytes;
 use std::{io, str};
 use std::net::ToSocketAddrs;
 
-use futures::{Future, Stream, Poll, Async};
-use futures::{Sink, AsyncSink, StartSend};
-use futures::future::{self, loop_fn, FutureResult, Loop, LoopFn};
+use futures::{Future, Poll, StartSend};
+use futures::{Async, Stream, Sink, AsyncSink};
+use futures::future::{loop_fn, Loop};
 use tokio_proto::TcpClient;
-use tokio_proto::pipeline::ClientProto;
+use tokio_proto::pipeline::{ClientProto, ClientService};
 use tokio_service::Service;
 use tokio_core::reactor::Core;
+use tokio_core::net::TcpStream;
 use tokio_io::codec::{Encoder, Decoder, Framed};
 use tokio_io::{AsyncRead, AsyncWrite};
 use bytes::BytesMut;
@@ -49,7 +50,6 @@ impl Encoder for LineCodec {
     type Error = io::Error;
 
     fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
-        //println!("Sending: {}", msg);
         buf.extend(msg.as_bytes());
         buf.extend(b"\n");
         Ok(())
@@ -80,7 +80,6 @@ impl<T> Stream for PingPong<T>
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Option<String>, io::Error> {
-        //loop {
         // Poll the upstream transport
         match try_ready!(self.upstream.poll()) {
             Some(ref msg) if msg.starts_with("PING ") => { 
@@ -96,11 +95,7 @@ impl<T> Stream for PingPong<T>
             },
             // Final output:
             m => Ok(Async::Ready(m))
-                //m => {
-            //    self.upstream.foo();
-            //},
         }
-        //}
     }
 }
 
@@ -111,7 +106,7 @@ impl<T> Sink for PingPong<T>
     type SinkError = io::Error;
 
     fn start_send(&mut self, item: String) -> StartSend<String, io::Error> {
-        // Only accept the write if there are no pending pongs
+        // Only accept the write if there are no pending pong
         if self.response.is_some() {
             return Ok(AsyncSink::NotReady(item));
         }
@@ -141,9 +136,6 @@ impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for LineProto {
 
 
 fn main() {
-    //let addr = "0.0.0.0:12345".parse().unwrap();
-    //TcpServer::new(IntProto, addr)
-    //    .serve(|| Ok(Doubler));
     let cm = "USER qj 0 * qjkx\r\nNICK qjk\r\nJOIN #test\r\n".to_string();
 
     //let addr = "irc.freenode.org:6697".to_socket_addrs().unwrap().next().unwrap();
@@ -153,104 +145,17 @@ fn main() {
     let handle = core.handle();
 
     let tc = TcpClient::new(LineProto);
-    /*
-    let response = tc
-        .connect(&addr, &handle)
-        .and_then(|client| {
-            client.call("ONE".to_string())
-                .and_then(move |response| {
-                    client.call("TWO".to_string())
-                        .and_then(move |response| {
-                            client.call("THREE".to_string())
-                        })
-                })
-        });
-        */
     let response = tc.connect(&addr, &handle)
-        .and_then(|client| {
-            client.call(cm)
-                .and_then(move |r0| {
-                    loop_fn(client, |c| {
-                        c.call("ack".to_string())
-                            .and_then(|ri| {
-                                if ri == "DONE" {
-                                    Ok(Loop::Break(c))
-                                } else {
-                                    Ok(Loop::Continue(c))
-                                }
-                            })
-                    })
+        .and_then(|client| client.call(cm)
+            .and_then(move |_r0| loop_fn(client, |c| c.call("ACK".to_string())
+                .and_then(|_ri| {
+                    let lcs: Loop<ClientService<TcpStream,LineProto>,_> 
+                        = Loop::Continue(c);
+                    Ok(lcs)
                 })
-        });
-    //let socket = tc.connect(&addr, &handle);
-    //let response = socket.and_then(|x| x.call(cm));
+            ))
+        );
     
-    let data = core.run(response);//.unwrap();
-    //println!("{:?}", data);
-
-    
-    //let client = tc.and_then(|x| x.call(cm));
-    //response.foo();
-    //response.and_then(|x| future::ok(x).foo());
-                    //client.call("BYE\r\n".to_string())
-
-    //let listen = cli.connect(&addr, &handle).and_then(|r| { });
-    //let listen = loop_fn(cli.connect(&addr, &handle), |msg| {
-    //let listen = loop_fn(socket.and_then(|s| s.call(cm)), |msg| {
-    
-    //let listen: LoopFn<Result<_,io::Error>, _> = loop_fn(conn, |cli| {
-        //cli.and_then(|x| cli.call());
-        //cli.foo();
-        //cli.and_then(|x| future::ok(x).foo())
-        //let r = cli.and_then(|x| x.call("HI\r\n"));
-        //let r = cli.call("HI\r\n");
-        //r.foo();
-    //});
-
-    //let mut a = core.run(listen).unwrap();
-    //a.poll();
-        /*
-    let listen: LoopFn<Result<_,io::Error>, _> = loop_fn(response, |x| {
-        k
-        x.and_then(|y| {
-            //y.call("BAD\r\n").and_then(
-            if y.contains("fuck") {
-                //Ok(Loop::Break(y))
-                Ok(Loop::Break(x))
-            } else {
-                Ok(Loop::Continue(x))
-            }
-        })
-        */
-    /*
-        if true {
-            Ok(Loop::Break(msg))
-        } else {
-            //Ok(Loop::Continue(msg))
-            Ok(Loop::Break(msg))
-        }
-    });
-        */
-
-        /*
-    let req = cli.connect(&addr, &handle)
-        .and_then(|client| {
-            client.call(cm)
-                .and_then(move |response| {
-                    println!("CLIENT: {:?}", response);
-                    client.call("BYE\r\n".to_string())
-                })
-            .and_then(|response| {
-                println!("CLIENT_: {:?}", response);
-                Ok(())
-            })
-        });
-        */
-        
-    //let response = socket.and_then(|x| x.call(cm));
-
-    //let data = core.run(response);//.unwrap();
-    //println!("{:?}", data);
-    //let (_socket, data) = core.run(response).unwrap();
-    //println!("{}", String::from_utf8_lossy(&data));
+    core.run(response).unwrap();
 }
+
