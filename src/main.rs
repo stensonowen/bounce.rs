46f3936c90fa4eaf2f001cf4f18fead0ea9108b8
owen <owen@oms.sh>
2017-04-27 21:49:57 -0400

    Ponging works; next up is message parsing / logging
    Small chance a client connection will require a major restructure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #[macro_use]
 extern crate futures;
-extern crate tokio_proto;
-extern crate tokio_service;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate bytes;
@@ -11,16 +9,10 @@ use std::net::ToSocketAddrs;
 
 use futures::{stream, Future, Poll, StartSend};
 use futures::{Async, Stream, Sink, AsyncSink};
-//use futures::future::{loop_fn, Loop};
-//use tokio_proto::TcpClient;
-//use tokio_proto::pipeline::{ClientProto, ClientService};
-use tokio_proto::pipeline::ClientProto;
-//use tokio_service::Service;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
-use tokio_io::codec::{Encoder, Decoder, Framed};
-use tokio_io::{AsyncRead, AsyncWrite};
-//use bytes::{BufMut, BytesMut};
+use tokio_io::codec::{Encoder, Decoder};
+use tokio_io::AsyncRead;
 use bytes::BytesMut;
 
 
@@ -32,14 +24,12 @@ impl Decoder for LineCodec {
     type Error = io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
-        //println!("Received: {:?}", std::str::from_utf8(buf));
         if let Some(i) = buf.iter().position(|&b| b == b'\n') {
             let line = buf.split_to(i);
             buf.split_to(1);
             match str::from_utf8(&line) {
                 Ok(s) => Ok(Some(s.to_string())),
-                Err(_) => Err(io::Error::new(io::ErrorKind::Other,
-                                             "invalid UTF-8")),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "bad utf8")),
             }
         } else {
             Ok(None)
@@ -61,8 +51,9 @@ impl Encoder for LineCodec {
 
 pub struct PingPong<T> {
     upstream: T,
-    response: Option<String>,
-    // TODO: stuff like AWAY ?
+    response: Option<String>
+    // TODO: `away` state?
+    //  others? 
 }
 
 impl<T> PingPong<T> {
@@ -76,7 +67,7 @@ impl<T> PingPong<T> {
 
 impl<T> Stream for PingPong<T>
     where T: Stream<Item = String, Error = io::Error>,
-          T: Sink<SinkItem = String, SinkError = io::Error>,
+          T: Sink<SinkItem = String, SinkError = io::Error>
 {
     type Item = String;
     type Error = io::Error;
@@ -84,8 +75,9 @@ impl<T> Stream for PingPong<T>
     fn poll(&mut self) -> Poll<Option<String>, io::Error> {
         // Poll the upstream transport
         match try_ready!(self.upstream.poll()) {
-            Some(ref msg) if msg.starts_with("PING ") => { 
+            Some(ref msg) if msg.starts_with("PING ") => {
                 // Intercept pings
+                println!("\tGETTING PING");
                 let resp = msg.replacen("PING", "PONG", 1);
                 self.response = Some(resp);
                 self.poll_complete()?;
@@ -94,29 +86,30 @@ impl<T> Stream for PingPong<T>
                 // does this actually work? never tested it
                 println!("NOTE: {:?}", poll);
                 Ok(Async::Ready(poll))
-            },
+            }
             // Final output:
-            m => Ok(Async::Ready(m))
+            m => Ok(Async::Ready(m)),
         }
     }
 }
 
 impl<T> Sink for PingPong<T>
-    where T: Sink<SinkItem = String, SinkError = io::Error>,
+    where T: Sink<SinkItem = String, SinkError = io::Error>
 {
     type SinkItem = String;
     type SinkError = io::Error;
 
     fn start_send(&mut self, item: String) -> StartSend<String, io::Error> {
         // Only accept the write if there are no pending pong
-        if self.response.is_some() {
-            return Ok(AsyncSink::NotReady(item));
+        match self.response {
+            Some(_) => Ok(AsyncSink::NotReady(item)),
+            None => self.upstream.start_send(item),
         }
-        self.upstream.start_send(item)
     }
 
     fn poll_complete(&mut self) -> Poll<(), io::Error> {
         if let Some(pong) = self.response.take() {
+            println!("\tSENDING PONG");
             self.upstream.start_send(pong)?;
         }
         self.upstream.poll_complete()
@@ -124,141 +117,31 @@ impl<T> Sink for PingPong<T>
 }
 
 
-
-pub struct LineProto;
-
-impl<T: AsyncRead + AsyncWrite + 'static> ClientProto<T> for LineProto {
-    type Request = String;
-    type Response = String;
-    type Transport = PingPong<Framed<T, LineCodec>>;
-    type BindTransport = Result<Self::Transport, io::Error>;
-    fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(PingPong::new(io.framed(LineCodec)))
-    }
-}
-
-
-
-
 fn main() {
-    let conn_msg: Vec<Result<String,io::Error>> = vec![
-        Ok("USER a b c d".to_string()),
-        Ok("NICK qjk".to_string()),
-        Ok("JOIN #test".to_string()),
+    let conn_msg: Vec<Result<String, io::Error>> = vec![
+        Ok("USER a b c d".to_string()), 
+        Ok("NICK qjkxk".to_string()),
+        Ok("JOIN #test".to_string())
     ];
 
-    //let addr = "irc.freenode.org:6667".to_socket_addrs().unwrap().next().unwrap();
-    let addr = "0.0.0.0:12345".to_socket_addrs().unwrap().next().unwrap();
+    let addr = "irc.freenode.org:6667".to_socket_addrs().unwrap().next().unwrap();
+    //let addr = "0.0.0.0:12345".to_socket_addrs().unwrap().next().unwrap();
 
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    //let p = PingPong::new(LineCodec);
 
-    let work = TcpStream::connect(&addr, &handle)
-        .and_then(|socket| {
-            // is this redundant?
-            let (sink, stream) = socket.framed(LineCodec).split();
-            //let (sink, stream) = socket.framed(LineProto).split();
-            //let (sink, stream) = socket.framed(PingPong<LineCodec>).split();
-            sink.send_all(stream::iter(conn_msg)).and_then(|_| {
-                // better way to do this?
+    let stream = TcpStream::connect(&addr, &handle);
+    let listen = stream.and_then(|socket| {
+        let transport = PingPong::new(socket.framed(LineCodec));
+        let (sink, stream) = transport.split();
+        sink.send_all(stream::iter(conn_msg))
+            .and_then(|_| {
                 stream.for_each(|i| {
-                    println!("SAW: `{}`", i.replace("\r\n", ""));
+                    println!("SAW: `{:?}`", i);
                     futures::future::ok(())
                 })
             })
-        });
-    // will be empty vec
-    let data = core.run(work).unwrap();
-    println!("DATA: {:?}", data);
-
-/*
-	let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let remote_addr = "127.0.0.1:14566".parse().unwrap();
-
-    let work = TcpStream::connect(&remote_addr, &handle)
-        .and_then(|socket| {
-            // Once the socket has been established, use the `framed` helper to
-            // create a transport.
-            let transport = socket.framed(LineCodec);
-
-            // We're just going to send a few "log" messages to the remote
-            let lines_to_send: Vec<Result<String, io::Error>> = vec![
-                Ok("Hello world".to_string()),
-                Ok("This is another message".to_string()),
-                Ok("Not much else to say".to_string()),
-            ];
-
-            // Send all the messages to the remote. The strings will be encoded by
-            // the `Codec`. `send_all` returns a future that completes once
-            // everything has been sent.
-            transport.send_all(stream::iter(lines_to_send))
-        });
-    core.run(work).unwrap();
-*/
-
-
-
-
-    //let tc = TcpClient::new(LineProto);
-
-    /*
-    let tc = TcpClient::new(LineProto);
-    let response = tc.connect(&addr, &handle)
-        .and_then(|client| client.call("USER a b c d".to_string())
-            .and_then(move |r1| {
-                println!("GOT1: {}", r1);
-                client.call("NICK qjkxk".to_string())}
-                .and_then(move |r2| {
-                    println!("GOT2: {}", r2);
-                    client.call("JOIN #test".to_string())}
-                    .and_then(move |r3| {
-                        println!("GOT3: {}", r3);
-                        client.call("PRIVMSG #test foo".to_string())}
-                              )
-                          )
-                      )
-            );
-            */
-
-    /*
-    let response = tc.connect(&addr, &handle)
-        .and_then(|client| client.call(cm)
-            //.and_then(|_r0| loop_fn(client, |c| c.call("ACK".to_string())
-            .and_then(|_r0| loop_fn(client, |c| futures::future::ok(()) // :/
-            //.and_then(|_r0| loop_fn(client, |c| futures::future::ok(c)
-                        .and_then(|_ri| {
-                            //x.fetch_add(1, Ordering::SeqCst);
-                            //println!("`{}`", _ri);
-                            let lcs: Loop<ClientService<TcpStream,LineProto>,_> 
-                                = Loop::Continue(c);
-                            Ok(lcs)
-                        })
-                    ))
-                );
-                      */
-    /*
-    let response = tc.connect(&addr, &handle)
-        .and_then(|client| client.foo().call("USER a b c d".to_string())
-            .and_then(|rx| client.call("NICK qjkxk".to_string())
-                .and_then(|ry| client.call("JOIN #test".to_string())
-            //.and_then(|_r0| loop_fn(client, |c| c.call("ACK".to_string())
-                    .and_then(|_r0| loop_fn(client, |c| c.call(String::new())
-            //.and_then(|_r0| loop_fn(client, |c| futures::future::ok(c)
-                        .and_then(|_ri| {
-                            x.fetch_add(1, Ordering::SeqCst);
-                            //println!("`{}`", _ri);
-                            let lcs: Loop<ClientService<TcpStream,LineProto>,_> 
-                                = Loop::Continue(c);
-                            Ok(lcs)
-                        })
-                    ))
-                )
-            )
-        );
-        */
-    
-    //core.run(response).unwrap();
+    });
+    // empty tuple
+    core.run(listen).unwrap();
 }
-
