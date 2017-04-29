739e55354e45ac9255f83d61b078ae3274c874d2
owen <owen@oms.sh>
2017-04-29 16:30:27 -0400

    Playing around w/ futures for file open/timeout

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,31 @@ use codec::{LineCodec, PingPong};
 use codec::line::Line;
 
 
+use tokio_timer::Timer;
+use std::time::{Duration};
+use std::thread;
+
 fn main() {
+    println!("test");
+    let f = futures::future::ok::<bool, tokio_timer::TimeoutError<()>>(false);
+    let g = f.and_then(|i| {
+        Timer::default()
+            .sleep(Duration::from_millis(1000))
+            .wait().unwrap();
+        futures::future::ok(true)
+    });
+    let timer = Timer::default();
+    let t = timer.sleep(Duration::from_millis(2000)).boxed()
+        .map_err(|e| tokio_timer::TimeoutError::Timer((), e))
+        .map(|e| false);
+    let w = t.select(g).map(|(a,_)| a);
+    let x = w.wait();
+    let r = x.unwrap_or(false);
+    println!("Action completed: {}", r);
+
+}
+
+fn _main() {
     let conn_msg: Vec<Result<Line, io::Error>> = vec![
         Ok(Line::from_str("USER a b c d")),
         Ok(Line::from_str("NICK qjkxk")),
