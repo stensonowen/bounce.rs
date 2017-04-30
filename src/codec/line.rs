0e3bf7c68b17ad5814ca191ae712dccc7fc290d2
owen <owen@oms.sh>
2017-04-29 23:22:52 -0400

    Basic logging works, but isn't finished
    Currently it manually flushes after every write because there's no other
    mechanism to close files. In the future there should be some sort of ~GC
    thread that periodically closes buffers that haven't been touched
    recently, and can maybe use tokio-timer::Interval.
    Next up is probably creating a config file with servers/channels, misc
    options, nick/info, etc.
    Also, is it going to be difficult to bounce for multiple servers? I
    don't think there can be multiple independent listeners on the same
    event loop(?). So there might need to be one thread per server. I guess
    that's reasonable

--- src/codec/line.rs
+++ src/codec/line.rs
@@ -1,10 +1,5 @@
 use std::string::ToString;
-use std::path::PathBuf;
-use std::io::{self, BufWriter};
-use std::fs::File;
-use std::collections::HashMap;
-
-use futures_cpupool::CpuPool;
+use time;
 
 #[derive(Debug)]
 pub enum Line {
@@ -35,6 +30,29 @@ impl Line {
         let s = p.replacen("PING ", "PONG ", 1);
         Line::Ping { orig: s }
     }
+    pub fn format_privmsg(&self) -> Option<(&str,String)> {
+        // (name,msg)
+        // if this message was in a public channel, `name` should be that channel
+        // if it was a private message from another user, it should be their nick
+        if let &Line::PrivMsg{ ref src, ref dst, ref text, .. } = self {
+            let now = time::now();
+            let msg = format!("{} {:>9}:  {}\n", now.rfc3339(), src, text);
+            // https://tools.ietf.org/html/rfc2812#section-2.3.1
+            let valid_nick_start = |c: char| 
+                c >= char::from(0x41) && c <= char::from(0x7d);
+            let name = if dst.starts_with(valid_nick_start) { src } else { dst };
+            Some((name,msg))
+        } else {
+            None
+        }
+    }
+    pub fn mention(&self, nick: &str) -> bool {
+        if let &Line::PrivMsg{ ref dst, ref text, .. } = self {
+            dst == nick || text.contains(nick)
+        } else {
+            false
+        }
+    }
     pub fn from_str(input: &str) -> Self {
         // TODO: adhere closer to the RFC
         // e.g. `:Angel!wings@irc.org PRIVMSG Wiz message goes here`
@@ -47,8 +65,8 @@ impl Line {
         let c = parts.nth(0);
         let d = parts.nth(0);
         match (a, b, c, d) {
-            (Some(s), Some("PRIVMSG"), Some(d), Some(m)) | 
-                (Some(s), Some("NOTICE"), Some(d), Some(m)) => 
+            //(Some(s), Some("NOTICE"), Some(d), Some(m)) |
+            (Some(s), Some("PRIVMSG"), Some(d), Some(m)) =>
             {
                 let i = if s.starts_with(':') { 1 } else { 0 };
                 let j = s.find('!').unwrap_or(s.len()-1);
@@ -72,167 +90,4 @@ impl ToString for Line {
     }
 }
 
-
-use tokio_timer::{Timer, Timeout, TimerError};
-use futures_cpupool::CpuFuture;
-use futures::Future;
-use std::io::Write;
-use std::thread;
-use std::time::Duration;
-use std::marker::Send;
-pub struct FileClosed;
-const FILE_TIMEOUT_MS: u64 = 1_000;
-
-// Maintain on-disk logs *in addition to* in-memory buffer
-// TODO: don't use a box
-pub enum FileState {
-    Open {
-        writer: BufWriter<File>,
-        //timeout: Box<Future<Item=io::Result<FileState>, Error=TimerError> + Send>,
-        timeout: Box<Future<Item=FileState, Error=TimerError> + Send>,
-        // can we chain onto this afterwards? p sure
-        //timer: Option<Timeout<CpuFuture<FileClosed, ()>>>,
-        path: PathBuf,
-    },
-    Closed(PathBuf),
-}
-
-impl FileState {
-    fn new(name: &str) -> Self {
-        FileState::Closed(PathBuf::from(name))
-    }
-    fn is_open(&self) -> bool {
-        if let &FileState::Open{..} = self { true } else { false }
-    }
-    fn clone_path(&self) -> PathBuf {
-        match self {
-            &FileState::Open { path: ref p, .. } => p,
-            &FileState::Closed(ref p) => p,
-        }.clone()
-    }
-    fn open(self) -> io::Result<FileState> {
-        if self.is_open() {
-            Ok(self)
-        } else {
-            let pb = self.clone_path();
-            let mut f = File::open(&pb)?;
-            let bw = BufWriter::new(f);
-            let timer = Timer::default();
-            let close_event = timer.sleep(Duration::from_millis(FILE_TIMEOUT_MS))
-                .then(|t| t.map(|_| self.close().unwrap()));
-            Ok(FileState::Open {
-                writer:  bw,
-                timeout: close_event.boxed(),
-                path:    pb,
-            })
-        }
-    }
-    fn close(self) -> io::Result<FileState> {
-        if let FileState::Open{ writer: mut w, path: pb, .. } = self {
-            w.flush()?;
-            Ok(FileState::Closed(pb))
-        } else {
-            Ok(self)
-        }
-    }
-}
-
-pub struct Logs(HashMap<String,FileState>);
-use futures;
-
-impl Logs {
-    pub fn new() -> Self {
-        Logs(HashMap::new())
-    }
-    pub fn add(&mut self, name: &str) {
-        // TODO: not clobber?
-        let val = FileState::new(name);
-        self.0.insert(name.to_string(), val);
-    }
-    pub fn open(&mut self, name: String) {
-        let old_val = self.0.remove(&name).unwrap_or(FileState::new(&name));
-        let new_val = if old_val.is_open() {
-            let tmp = old_val.open().unwrap();
-            if let FileState::Open{ timeout: ref t, .. } = tmp {
-                t.and_then(|x| futures::future::ok(()));
-
-            }
-            tmp
-        } else {
-            old_val
-            //self.0.insert(name.to_string(), old_val);
-        };
-        self.0.insert(name, new_val);
-        //let entry = self.0.entry(name.to_string())
-        //    .or_insert(FileState::new(name));
-        //if entry.is_open() == false {
-        //    let val = entry.open();
-        //}
-    }
-
-}
-/*
-pub struct Logs {
-    files: HashMap<String,FileState>,
-    pool: CpuPool,
-//(HashMap<String,FileState>);
-}
-
-impl Logs {
-    pub fn new() -> Self {
-        Logs {
-            files: HashMap::new(),
-            pool: CpuPool::new(2),
-        }
-    }
-
-}
-*/
-/*
-impl Logs {
-    pub fn new() -> Self {
-        Logs {
-            files: HashMap::new(),
-            pool: CpuPool::new(2),
-        }
-    }
-    pub fn add(&mut self, name: &str) {
-        if self.files.contains_key(name) == false {
-            let val = FileState::Closed(PathBuf::from(name));
-            self.files.insert(name.to_string(), val);
-        } else {
-            panic!("Value duplicated");
-        }
-    }
-    pub fn open(&mut self, name: &str) -> io::Result<&BufWriter<File>> {
-        let mut file = File::open(name)?;
-        let val = FileState::Open(BufWriter::new(file));
-        self.files.insert(name.to_owned(), val);
-        match self.files.get(name) {
-            Some(&FileState::Open(ref br)) => Ok(br),
-            _ => unreachable!(),
-        }
-    }
-    pub fn write_and_close(&mut self, name: &str) -> io::Result<()> {
-        let g = self.files.get(name);
-        let w = match g {
-            Some(&FileState::Open(ref b)) => b,
-            Some(&FileState::Closed(_)) => {
-                self.open(name)?
-            },
-            None => {
-                self.add(name);
-                self.open(name)?
-            }
-        };
-        Ok(())
-    }
-    //pub fn new() -> Self {
-    //    Logs(HashMap::new())
-    //}
-    //pub fn update(line: &Line) -> Result<usize,io::Error> {
-    //    Ok(0)
-    //}
-}
-*/
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate futures;
-extern crate futures_cpupool;
 extern crate tokio_io;
 extern crate tokio_core;
 extern crate tokio_timer;
@@ -10,98 +9,25 @@ extern crate time;
 use std::{io, str};
 use std::net::ToSocketAddrs;
 
-use futures::{stream, Future, Stream, Sink, IntoFuture};
+use futures::{stream, Future, Stream, Sink};
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
 use tokio_io::AsyncRead;
 
+pub mod log;
 pub mod codec;
 use codec::{LineCodec, PingPong};
 use codec::line::Line;
-
-use tokio_timer::Timer;
-use std::time::{Duration};
-use std::thread;
-use futures_cpupool::CpuPool;
-
-//const FILE_TIMEOUT_MS: u64 = 1_000;
-
-
-use std::io::BufWriter;
-use std::fs::File;
-use std::io::Write;
-use futures_cpupool::CpuFuture;
-
-struct FileClosed;
-
-//fn close_handle(mut f: File) {
-fn close_handle() -> FileClosed {
-    let mut f = File::create("test.txt").unwrap();
-    thread::sleep(Duration::from_millis(1000));
-    f.write(b"Hello world\n");
-    f.flush().unwrap();
-    FileClosed
-}
+use log::Logs;
 
 fn main() {
-    //close_handle();
-    let pool = CpuPool::new(2);
-    let timer = Timer::default();
-        //let timeout = timer.sleep(Duration::from_millis(750)).then(|_| Err(()));
-        //let func = pool.spawn_fn(|| Ok(close_handle()));
-        //let winner = timeout.select(func).map(|(win, _)| win);
-        //func.foo();
-    //let func: CpuFuture<_,()> = pool.spawn_fn(|| Ok(close_handle()));
-    //let timeout = timer.timeout(func, Duration::from_millis(900));
-        //timeout.foo();
-        //timeout.wait();
-    let a = timer.sleep(Duration::from_millis(500))
-        .then(|x| x.map(|_| close_handle())).boxed();
-
-    //a.foo();
-        //.and_then(|_| futures::future::ok(()));
-
-    //thread::sleep(Duration::from_millis(1200));
-
-    //let action = futures::future::ok::<_, tokio_timer::TimeoutError<_>>(false)
-    //    .and_then(|i| {
-    //        Timer::default()
-    //            .sleep(Duration::from_millis(1000))
-    //            .wait().unwrap();
-    //        futures::future::ok(true)
-    //});
-        //let timer = Timer::default();
-    //let f = timer.timeout(action, Duration::from_millis(2000));
-    //f.foo();
-    //thread::sleep(Duration::from_millis(5_000));
-}
-
-fn main2() {
-    println!("test");
-    let f = futures::future::ok::<bool, tokio_timer::TimeoutError<()>>(false);
-    let g = f.and_then(|i| {
-        Timer::default()
-            .sleep(Duration::from_millis(1000))
-            .wait().unwrap();
-        futures::future::ok(true)
-    });
-    let timer = Timer::default();
-    let t = timer.sleep(Duration::from_millis(2000)).into_future()
-        .map_err(|e| tokio_timer::TimeoutError::Timer((), e))
-        .map(|e| false);
-    let w = t.select(g).map(|(a,_)| a);
-    let x = w.wait();
-    let r = x.unwrap_or(false);
-    println!("Action completed: {}", r);
-
-}
-
-fn _main() {
     let conn_msg: Vec<Result<Line, io::Error>> = vec![
         Ok(Line::from_str("USER a b c d")),
         Ok(Line::from_str("NICK qjkxk")),
         Ok(Line::from_str("JOIN #test")),
     ];
+    let log_path = "/tmp/irc_logs/";    // must exist and be writable
+    let mut logs = Logs::new(log_path); 
 
     let addr = "irc.freenode.org:6667".to_socket_addrs().unwrap().next().unwrap();
     //let addr = "0.0.0.0:12345".to_socket_addrs().unwrap().next().unwrap();
@@ -115,8 +41,11 @@ fn _main() {
         let (sink, stream) = transport.split();
         sink.send_all(stream::iter(conn_msg))
             .and_then(|_| {
-                stream.for_each(|i| {
-                    println!("SAW: `{:?}`", i);
+                stream.for_each(|line| {
+                    println!("SAW: `{:?}`", line);
+                    if let Some((name,text)) = line.format_privmsg() {
+                        logs.write(name,&text).unwrap();
+                    }
                     futures::future::ok(())
                 })
             })
