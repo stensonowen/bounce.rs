11e0812e382267e83e5a0cea2596be5f23fe5894
owen <owen@oms.sh>
2017-07-23 17:15:15 -0400

    Refactored configuration: cleaner and far fewer allocs

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,14 @@
+/* TODO: when(if?) serde supports it, deserialize arrays to `&'a [&'a str]` 
+ *  instead of `Vec<&'a str>`
+ */
+
 use std::collections::HashMap;
 use std::io::{self, Read};
+use std::path::{Path, PathBuf};
+use std::net::{SocketAddr, ToSocketAddrs};
 use std::fs::File;
-use rpassword;
 use toml;
+use std::borrow::Cow;
 
 const DEFAULT_UTC: bool = false;
 const DEFAULT_TLS: bool = false;
@@ -10,182 +16,131 @@ const DEFAULT_LOG: bool = true;
 const DEFAULT_MODE: u8 = 0;
 
 
+#[derive(Deserialize)]
+struct ConfigToml<'a> {
+    nick:       Option<&'a str>,
+    nick_alt:   Option<&'a str>,
+    logs_dir:   &'a str,
+    timefmt:    Option<&'a str>,
+    utc:        Option<bool>,
+    tls:        Option<bool>,
+    user:       Option<&'a str>,
+    mode:       Option<u8>,
+    realname:   Option<&'a str>,
+    servers:    HashMap<&'a str, ServerToml<'a>>,
+}
 
 #[derive(Deserialize)]
-struct ConfigToml {
-    nick: Option<String>,
-    logs_dir: String,
-    timefmt: Option<String>,
-    utc: Option<bool>,
-    user: Option<String>,
-    mode: Option<u8>,
-    realname: Option<String>,
-    servers: HashMap<String,ServerToml>,
-}
-
-#[derive(Deserialize)]
-struct ServerToml {
-    addr: String,
-    chans: Vec<String>,
-    chan_keys: Option<Vec<String>>,
-    tls: Option<bool>,
-    password: Option<bool>,
-    register: Option<bool>,
-    nick: Option<String>,
-    log: Option<bool>,
-    user: Option<String>,
-    mode: Option<u8>,
-    realname: Option<String>,
+struct ServerToml<'a> {
+    addr:       &'a str,
+    chans:      Vec<&'a str>,
+    chan_keys:  Option<Vec<&'a str>>,
+    tls:        Option<bool>,
+    password:   Option<bool>,
+    register:   Option<bool>,
+    nick:       Option<&'a str>,
+    nick_alt:   Option<&'a str>,
+    log:        Option<bool>,
+    user:       Option<&'a str>,
+    mode:       Option<u8>,
+    realname:   Option<&'a str>,
 }
 
 #[derive(Debug)]
-pub struct Config {
-    pub logs_dir: String,
-    pub timefmt: Option<String>,
-    pub servers: HashMap<String,Server>,
-    pub utc: bool,
+pub struct Config<'a> {
+    pub logs_dir:   PathBuf,
+    pub timefmt:    Option<&'a str>,
+    pub servers:    HashMap<&'a str, Server<'a>>,
+    pub utc:        bool,
 }
 
 #[derive(Debug)]
-pub struct Server {
-    pub nick: String,
-    pub user: String,
-    pub mode: u8,
-    pub realname: String,
-    addr: String,
-    pub chans: Vec<String>,
-    pub chan_keys: Option<Vec<String>>,
-    pub password: Option<String>,
-    pub register: Option<String>,
-    pub tls: bool,
+pub struct Server<'a> {
+    pub nick:       &'a str,
+    pub nick_alt:   Option<&'a str>,
+    pub user:       &'a str,
+    pub mode:       u8,
+    pub realname:   &'a str,
+    pub addr:       SocketAddr,
+    pub chans:      Vec<&'a str>,
+    pub chan_keys:  Option<Vec<&'a str>>,
+    pub password:   Option<String>,
+    pub register:   Option<String>,
+    pub tls:        bool,
+    pub log:        bool,
 }
 
 #[derive(Debug)]
-pub enum ParseError {
+pub enum ConfigError<'a> {
     ReadError(io::Error),
     ParseError(toml::de::Error),
     ResolveError(String),
+    MissingData { field: &'static str, from: &'a str },
+    BadAddress,
 }
 
-use std::error::Error;
-impl ServerToml {
-    fn build(self, 
-             serv_name: &str,
-             log_dir: &str,
-             alt_n: &Option<String>, 
-             alt_u: &Option<String>,
-             alt_m: &Option<u8>, 
-             alt_r: &Option<String>,
-             ) -> Result<Server,String> {
-        let server_pw = if self.password.unwrap_or(false) {
-            let prompt = format!("Enter server password for {}: ", serv_name);
-            let pw = rpassword::prompt_password_stdout(&prompt)
-                .map_err(|e| e.description().to_owned())?;
-            Some(pw)
-        } else {
-            None
-        };
-        let regis_pw = if self.register.unwrap_or(false) {
-            let prompt = format!("Enter registration password for {}: ", serv_name);
-            let pw = rpassword::prompt_password_stdout(&prompt)
-                .map_err(|e| e.description().to_owned())?;
-            Some(pw)
-        } else {
-            None
-        };
+impl<'a> Config<'a> {
+    pub fn load_to_string<P: AsRef<Path>>(path: P, s: &'a mut String) 
+            -> Result<Config<'a>,ConfigError<'a>> {
+        use self::ConfigError::*;
+        let mut f = File::open(path).map_err(ReadError)?;
+        f.read_to_string(s).map_err(ReadError)?;
 
-        Ok(Server {
-            nick: self.nick.or(alt_n.clone())
-                .ok_or(format!("`Nick` field missing from {}", serv_name))?,
-            user: self.user.or(alt_u.clone())
-                .ok_or(format!("`User` field missing from {}", serv_name))?,
-            mode: self.mode.or(alt_m.clone()).unwrap_or(DEFAULT_MODE),
-            realname: self.realname.or(alt_r.clone())
-                .ok_or(format!("`Realname` field missing from {}", serv_name))?,
-            addr: self.addr,
-            chans: self.chans,
-            chan_keys: self.chan_keys,
-            password: server_pw,
-            register: regis_pw,
-            tls: self.tls.unwrap_or(DEFAULT_TLS),
-        })
+        let config: ConfigToml = toml::from_str(s).map_err(ParseError)?;
+        config.build()
     }
 }
 
-fn build_servers(olds: HashMap<String,ServerToml>, 
-                 alt_nick: Option<String>, 
-                 alt_user: Option<String>, 
-                 alt_mode: Option<u8>, 
-                 alt_real: Option<String>,
-                 log_dir: &str,
-                 ) -> Result<HashMap<String,Server>,String> {
-    let mut servers = HashMap::new();
-    for (name,serv) in olds {
-        let new = serv.build(&name, log_dir, &alt_nick, &alt_user, &alt_mode, &alt_real)?;
-        servers.insert(name.to_string(),new);
-    }
-    Ok(servers)
-}
-
-impl ConfigToml {
-    fn build(self) -> Result<Config,String> {
-        Ok(Config {
-            servers: build_servers(self.servers, self.nick, self.user, 
-                                   self.mode, self.realname, &self.logs_dir)?,
-            logs_dir: self.logs_dir,
-            timefmt: self.timefmt,
-            utc: self.utc.unwrap_or(DEFAULT_UTC),
-        })
-    }
-}
-
-impl Server {
-    pub fn conn_msg(&self) -> Vec<String> {
-        let mut cm = Vec::new();
-        if let Some(ref srv_pw) = self.password {
-            cm.push(format!("PASSWORD {}", srv_pw));
-        }
-        cm.push(format!("USER {} {} * {}", self.user, self.mode, self.realname));
-        cm.push(format!("NICK {}", self.nick));
-        if let Some(ref keys) = self.chan_keys {
-            cm.push(format!("JOIN {} {}", self.chans.join(","), keys.join(",")));
-        } else {
-            cm.push(format!("JOIN {}", self.chans.join(",")));
-        }
-        if let Some(ref reg_pw) = self.register {
-            cm.push(format!("PRIVMSG NickServ IDENTIFY {} {}", self.nick, reg_pw));
-        }
-        cm
-    }
-    pub fn get_addr(&self) -> String {
-        if self.addr.contains(':') {
-            self.addr.clone()
-        } else {
-            let mut s = self.addr.clone();
-            s.push(':');
-            if self.tls {
-                s.push_str("6697");
+impl<'a> ConfigToml<'a> {
+    fn build(self) -> Result<Config<'a>,ConfigError<'a>> {
+        use rpassword::prompt_password_stdout;
+        use self::ConfigError::*;
+        let mut servers: HashMap<&'a str, Server> = HashMap::new();
+        for (name,srv) in self.servers {
+            let server_pw = if srv.password.unwrap_or(false) {
+                let prompt = format!("Enter server password for {}: ", name);
+                let pw = prompt_password_stdout(&prompt).map_err(ReadError)?;
+                Some(pw)
             } else {
-                s.push_str("6667");
-            }
-            s
-        }
-    }
-    /*
-    pub fn get_dir(&self) -> &Path {
-        Path::new(self.log_path.unwrap())
-    }
-    */
-}
+                None
+            };
+            let regis_pw = if srv.register.unwrap_or(false) {
+                let prompt = format!("Enter registration password for {}: ", name);
+                let pw = prompt_password_stdout(&prompt).map_err(ReadError)?;
+                Some(pw)
+            } else {
+                None
+            };
+            let from = name;
+            let nick = srv.nick.or(self.nick).ok_or(MissingData{field:"Nick", from})?;
+            let user = srv.user.or(self.user).ok_or(MissingData{field:"User", from})?;
+            let mode = srv.mode.or(self.mode).unwrap_or(DEFAULT_MODE);
+            let real = srv.realname.or(self.realname).unwrap_or(user);
+            let tls = srv.tls.or(self.tls).unwrap_or(DEFAULT_TLS);
+            let nick_alt = srv.nick_alt.or(self.nick_alt);
 
-impl Config {
-    pub fn from(path: &str) -> Result<Config,ParseError> {
-        use self::ParseError::*;
-        let mut f = File::open(path).map_err(|e| ReadError(e))?;
-        let mut s = String::new();
-        f.read_to_string(&mut s).map_err(|e| ReadError(e))?;
-        let config: ConfigToml = toml::from_str(&s).map_err(|e| ParseError(e))?;
-        Ok(config.build().map_err(|e| ResolveError(e))?)
+            let addr_s: Cow<str> = if srv.addr.contains(':') {
+                Cow::Borrowed(srv.addr)
+            } else {
+                Cow::Owned(format!("{}:{}", srv.addr, if tls {6697} else {6667}))
+            };
+            let mut addr_i = addr_s.to_socket_addrs().map_err(ReadError)?;
+            let addr = addr_i.nth(0).ok_or(BadAddress)?;
+
+            let server = Server { nick, nick_alt, user, mode, addr, 
+                realname: real, chans: srv.chans, chan_keys: srv.chan_keys, 
+                password: server_pw, register: regis_pw, 
+                tls: srv.tls.unwrap_or(DEFAULT_TLS),
+                log: srv.log.unwrap_or(DEFAULT_LOG),
+            };
+            servers.insert(name,server);
+        }
+
+        Ok(Config {     servers,
+            logs_dir:   PathBuf::from(self.logs_dir),
+            timefmt:    self.timefmt,
+            utc:        self.utc.unwrap_or(DEFAULT_UTC),
+        })
     }
 }
 
